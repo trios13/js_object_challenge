104793090130afe3d3b6da464de213ec8c7038b2
Troy Rios <troy.rios@gmail.com>
2019-10-14 15:01:23 +0000

    initial Commit

---
+++ js/students.js
@@ -0,0 +1,32 @@
+var students = [
+  {
+    name: 'Troy Rios',
+    track: 'Front-End Web Development',
+    achievments: 13,
+    points: 689
+  },
+  {
+    name: 'Debbie Worthing',
+    track: 'iOS
+    achievments: 40,
+    points: 6969
+  },
+  {
+    name: 'Cassiah Allison',
+    track: 'Web Design',
+    achievments: 18,
+    points: 6913
+  },
+  {
+    name: 'Keturah Duffin',
+    track: 'Mobile Development'
+    achievments: 25,
+    points: 1013
+  },
+  {
+    name: 'Dustin Rios',
+    track: 'CMS Development',
+    achievments: 55,
+    points: 3056
+  },
+];
