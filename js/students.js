0e15fde4c8c29b40135d9d5de5ff10513257d8c0
Troy Rios <troy.rios@gmail.com>
2019-10-14 15:25:07 +0000

    challenge part 2

--- js/students.js
+++ js/students.js
@@ -7,7 +7,7 @@ var students = [
   },
   {
     name: 'Debbie Worthing',
-    track: 'iOS
+    track: 'iOS Development',
     achievments: 40,
     points: 6969
   },
@@ -19,7 +19,7 @@ var students = [
   },
   {
     name: 'Keturah Duffin',
-    track: 'Mobile Development'
+    track: 'Mobile Development',
     achievments: 25,
     points: 1013
   },
@@ -30,3 +30,18 @@ var students = [
     points: 3056
   },
 ];
+  
+function print(message) {
+  document.write(message);
+}
+  
+ var message = '';
+ for(var prop in students)
+{
+   message = '<h2>Name: ' + students[prop].name +'</h2>';
+   message += '<p>track: ' + students[prop].track + '</p>';
+   message += '<p>achievments: ' + students[prop].achievments + '</p>';
+   message += '<p>points: ' + students[prop].points + '</p>';
+   print(message);
+}
+  
